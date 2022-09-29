6ce16dcda743a0e5fab2046914812ed9d3d2e542
Alexandre Coreixas <coreixas@iMac-de-Hugo.local>
2022-09-29 13:49:57 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function preload(){ // função que carregar todas as imagens e animações
+  
+}
+
+function setup(){ // todas as configuraçoes dos objetos
+  createCanvas(600,200);
+  
+}
+
+function draw(){
+  background("black");
+  drawSprites(); 
+
+
+}
+
